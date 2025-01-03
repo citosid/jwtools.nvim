a5b430f31e144ce3753f4f592384abf57a9312ca
Abraham Cruz Sustaita <abraham.sustaita@gmail.com>
2025-01-03 00:19:39 -0500

    chore: add ability to access modules dynamically

--- lua/jwtools/init.lua
+++ lua/jwtools/init.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+setmetatable(M, {
+	__index = function(t, k)
+		---@diagnostic disable-next-line: no-unknown
+		t[k] = require("jwtools." .. k)
+		return rawget(t, k)
+	end,
+})
+
 M.didsetup = false
 
 function M.setup()
@@ -9,7 +17,7 @@ function M.setup()
 
 	M.didsetup = true
 
-	vim.api.nvim_create_user_command("JWToolsFetchScripture", require("jwtools.fetch").fetch_scripture, {})
+	vim.api.nvim_create_user_command("JWToolsFetchScripture", M.fetch.fetch_scripture, {})
 end
 
 return M
